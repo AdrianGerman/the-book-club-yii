b939b2eece3cc884c33f2f9216a3f3b25217896a
AdrianGerman <adrian.german1019@gmail.com>
2024-08-15 08:28:58 -0600

    creating the last database tables

--- schema/schema.sql
+++ schema/schema.sql
@@ -14,3 +14,41 @@ create table if not exists authors (
     name varchar(100) not null,
     nationality varchar(2)
 )   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+create table if not exists clubs (
+    club_id integer unsigned primary key auto_increment,
+    name varchar(100) not null unique,
+    description varchar(500),
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp on update current_timestamp
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+create table if not exists club_members (
+    club_member_id integer unsigned primary key auto_increment,
+    user_id integer unsigned not null,
+    club_id integer unsigned not null,
+    is_admin tinyint not null default 0,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp on update current_timestamp,
+    unique key no_rep(user_id, club_id)
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+create table if not exists user_books (
+    user_books_id integer unsigned primary key auto_increment,
+    user_id integer not null,
+    book_id integer not null,
+    created_at timestamp not null default current_timestamp,
+    modified_at timestamp not null default current_timestamp
+        on update current_timestamp,
+    unique key no_rep(user_id, book_id)
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+create table if not exists book_scores (
+	book_score_id integer unsigned primary key auto_increment,
+	user_id integer unsigned not null,
+	book_id integer unsigned not null,
+	score tinyint unsigned,
+	created_at timestamp not null default current_timestamp,
+	modified_at timestamp not null default current_timestamp on update current_timestamp,
+	unique key no_rep(user_id, book_id)
+)   ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
