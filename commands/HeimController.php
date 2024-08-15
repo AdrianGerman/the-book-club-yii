57165bc2091667d51a6a9637b57001de41a76296
AdrianGerman <adrian.german1019@gmail.com>
2024-08-15 14:39:21 -0600

    model author and function getAuthor

--- commands/HeimController.php
+++ commands/HeimController.php
@@ -7,6 +7,7 @@ use yii\console\Controller;
 use yii\console\ExitCode;
 
 use app\models\Book;
+use app\models\Author;
 
 class HeimController extends Controller
 {
@@ -26,14 +27,32 @@ class HeimController extends Controller
         while (!feof($f)) {
             $data = fgetcsv($f);
             if (!empty($data[1]) && !empty($data[2])) {
+                $author = Author::find()->where(['name' => $data[2]])->one();
+                if (empty($author)) {
+                    $author = new Author;
+                    $author->name = $data[2];
+                    $author->save();
+                }
+
                 $book = new Book;
                 $book->title = $data[1];
-                $book->author_id = 1;
+                $book->author_id = $author->id;
                 $book->save();
                 printf("%s\n", $book->toString());
             }
         }
         fclose($f);
+        return ExitCode::OK;
+    }
+
+    public function actionGetAuthor($author_id)
+    {
+        $author = Author::findOne($author_id);
+        if (empty($author)) {
+            printf("No existe el autor\n");
+            return ExitCode::DATAERR;
+        }
+        printf("Nombre: %s\n", $author->name);
         return ExitCode::OK;
     }
 }
